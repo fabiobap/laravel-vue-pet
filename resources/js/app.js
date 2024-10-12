2f00985b46c44beac07f4daa56ead4489e15522c
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-11 23:20:09 -0300

    adding moment js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import vuetify from './plugins/vuetify'
+
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +22,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
